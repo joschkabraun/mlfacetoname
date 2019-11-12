ae4620cc9f76ad06de00b17765659729e809abea
Joschka Braun <joschka.braun@gmail.com>
2019-11-12 18:18:39 -0500

    Added documentation

--- calculate_lfw_embeddings.py
+++ calculate_lfw_embeddings.py
@@ -1,5 +1,3 @@
-# oriented at https://machinelearningmastery.com/how-to-perform-face-recognition-with-vggface2-convolutional-neural-network-in-keras/
-
 import math
 import numpy as np
 from numpy import expand_dims
@@ -13,8 +11,17 @@ from keras_vggface.utils import decode_predictions
 import os
 import pickle
 
-# extract a single face from a given photograph
 def extract_face(filename, required_size=(224, 224)):
+    """Extracts a single face from a given photograph using MTCNN.
+
+    Parameters
+    ----------
+    filename : str
+        The filename of which the face shall be extracted
+    required_size : touple of int, optional
+        The size which the extracted face shall have (default is (224, 224))
+    """
+    
     # load image from file
     pixels = pyplot.imread(filename)
     # create the detector, using default weights
@@ -32,8 +39,17 @@ def extract_face(filename, required_size=(224, 224)):
     face_array = asarray(image)
     return face_array
 
-# extract faces and calculate face embeddings for a list of photo files
 def get_embeddings(filenames):
+    """Given a list of file names it extracts first the face and then computes their embeddings.
+
+    This method uses VGGFace resnet50 model (which version of VGGNet?)
+    
+    Parameters
+    ----------
+    filenames : list of str
+        The list of file names of which embeddings shall be calculated
+    """
+    
     faces = []
     
     # extract faces
@@ -53,8 +69,17 @@ def get_embeddings(filenames):
     yhat = model.predict(samples)
     return yhat
 
-# get all the paths of all files (including subdirectories) of given format
 def get_paths_of_files_of_format(path, file_format="jpg"):
+    """Finds all the paths of files with specified file format in all subdirectories of the specified path.
+
+    Parameters
+    ----------
+    path : str
+        The path of the starting directory
+    file_format : str, optional
+        The file format of which the all files shall be found (default is "jpg")
+    """
+    
     files = []
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
@@ -64,8 +89,19 @@ def get_paths_of_files_of_format(path, file_format="jpg"):
     
     return files
 
-# extract names of the images in the used files and composes them as (first name) (given name) (i.e. with a space)
 def extract_full_names(path_files,dataset="lfw"):
+    """Extract the full names of the given files.
+
+    This function was specifically written for the Labeled Faces of the Wild Dataset (LFW).
+
+    Parameters
+    ----------
+    path_files : str
+        The file paths of which the full names shall be extracted
+    dataset : str, optional
+        The dataset which is examined (default is "lfw")
+    """
+    
     names = []
     for f in path_files:
         fname = f.split("\\")[-1]              # the files always were separated by "\\" in the end
@@ -77,9 +113,34 @@ def extract_full_names(path_files,dataset="lfw"):
             raise NotImplementedError("Only code for the lfw dataset have been implemented")
     return names
 
-# averages vectors to associated name; expects numpy vectors as input
 class NamedAvgVector:
+    """A class used to combine a name, a vector (the embedding) and the number of used vectors in one data structure.
+
+    Attributes
+    ----------
+    name : str
+        The name
+    vec : numpy array of int
+        The average vector
+    n : int
+        The number of used vectors
+
+    Methods
+    -------
+    update(numpy_vec)
+        Updates the average vector and used vectors with the given numpy vector
+    """
+    
     def __init__(self, name, numpy_vec="a"):
+        """
+        Parameters
+        ----------
+        name : str
+            The name
+        numpy_vec : numpy vector of int, optional
+            The initialized average vector (default is "a" to declare it is unitialized)
+        """
+        
         self.name = name
         self.vec = numpy_vec
         if numpy_vec != "a":
@@ -88,6 +149,14 @@ class NamedAvgVector:
             self.n = 0
     
     def update(self, numpy_vec):
+        """Updates the average vector and used vectors with the given numpy vector
+
+        Parameters
+        ----------
+        numpy_vec : numpy array of int
+            The vector with which the average vector shall be updated
+        """
+        
         if self.n != 0:
             self.vec *= self.n
             self.vec += numpy_vec
@@ -97,35 +166,62 @@ class NamedAvgVector:
             self.n = 1
             self.vec = numpy_vec
             
-# creates a dictionary with all the first names as keys and associated empty NamedAvgVector data structures
 def create_initial_dict(full_names):
+    """Returns a dictionary of NamedAvgVector of the first name for the given names with unitialized average vector.
+
+    Parameters
+    ----------
+    full_names : list of str
+        The list of full names for which a dictionary of NamedAvgVector shall be created
+    """
+    
     dict_list = []
     for name in full_names:
         first_name = name.split(" ")[0]
         dict_list.append((first_name, NamedAvgVector(first_name)))
     return dict(dict_list)
 
-# updates the dictionary of NamedAvgVectors with the embeddings
-# assumes that all embeddings are already in dictionary
-# i_0_f_names : is the starting point of the calculated embeddings in full_names
 def update_dict(d, full_names, i_0_f_names, emb):
+    """Updates the dictionary of NamedAvgVector at the specified position with the given embeddings
+
+    Parameters
+    ----------
+    d : dict of NamedAvgVector
+        The to be updated dictionary of NamedAvgVector
+    full_names : list of str
+        The list of full names used in the dictionary (possibly have repetitions)
+    i_0_f_names : int
+        The starting index in full_names where the embeddings were calculated
+    emb : list of numpy array of int
+        The calculated embeddings which shall be used for the update
+    """
+    
     for i in range(len(emb)):
         fname = full_names[i_0_f_names + i].split(" ")[0]
         d[fname].update(np.array(emb[i]))
         
-# calculates the averaged embeddings for all the first names given a dataset
 def calculate_embeddings(path_dataset, step_size=100, file_format="jpg", dataset_name="lfw"):
+    """Calculates the averaged embeddings for all the first names given a dataset
+
+    Parameters
+    ----------
+    path_dataset : str
+        The path of the dataset
+    step_size : int, optional
+        The number of embeddings which shall be calculated for every update (default is 100)
+    file_format : str, optional
+        The used file format of the images (default is "jpg")
+    dataset_name : str, optional
+        The underlying used data set (default is "lfw")
+    """
+    
     im_paths = get_paths_of_files_of_format(path_lfw, file_format)
     size = len(im_paths)
     full_names = extract_full_names(im_paths)
 
-    #dict_navecs = create_initial_dict(full_names)
-    num = 131
-    f = open("dict_{}.pkl".format(num), "rb")
-    dict_navecs = pickle.load(f)
-    f.close()
+    dict_navecs = create_initial_dict(full_names)
     
-    for i in range(num, math.ceil(size/step_size)):
+    for i in range(math.ceil(size/step_size)):
         print("calculate_embeddings i:", i)
         im_paths_tmp = im_paths[i*step_size : (i+1)*step_size]
         emb_tmp = get_embeddings(im_paths_tmp)
